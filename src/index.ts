ebbc52454f0cd86a2331addbd7109113ce902bd1
Lukasz <7lukasz8@gmail.com>
2024-07-14 19:36:05 +0200

    mongodb config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
-import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from "path";
+import app from "./app";
 
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + dupadsd");
+const result_config = dotenv.config({
+  path: path.resolve(__dirname, "./config.env"),
 });
 
+if (result_config.error) {
+  console.error("Error:", result_config.error.message);
+  process.exit(1);
+}
+
+const DB = process.env.DATABASE!.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD!
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("MongoBD connection successful");
+  })
+  .catch((err: any) => {
+    console.error("Failed to connect with MongoDB: ", err);
+    process.exit(1);
+  });
+
+const port = process.env.PORT;
+
+if (!port) {
+  console.error("Error: PORT is not defined in the .env file.");
+  process.exit(1);
+}
+
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`App running on port ${port}: http://localhost:${port}/`);
 });
